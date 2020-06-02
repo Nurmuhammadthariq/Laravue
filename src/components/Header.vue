b84bc5d8ec73ef3c7e4e5e5c7f078baaf5476104
Nurmuhammadthariq <thoriq.noer@gmail.com>
2020-06-02 09:34:48 +0700

    Check

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
         <div class="container">
           <div class="ht-left">
             <div class="mail-service">
-              <i class=" fa fa-envelope"></i> hello.shayna@gmail.com
+              <i class=" fa fa-envelope"></i> Tokoonline@gmail.com
             </div>
             <div class="phone-service">
               <i class=" fa fa-phone"></i> +628 22081996
